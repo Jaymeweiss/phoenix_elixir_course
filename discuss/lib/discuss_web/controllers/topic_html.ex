2d7f895e1b9cf6d4888c4b61fffcb57c412bfb8b
Jayme Weiss <jayme.weiss@zappistore.com>
2023-09-25 19:45:23 +0200

    switch to core components

--- discuss/lib/discuss_web/controllers/topic_html.ex
+++ discuss/lib/discuss_web/controllers/topic_html.ex
@@ -1,7 +1,5 @@
 defmodule DiscussWeb.TopicHTML do
   use DiscussWeb, :html
-  import Phoenix.HTML.Form # Needed for form_for/4
-  alias DiscussWeb.Router.Helpers, as: Routes
 
   embed_templates "topic_html/*"
 end
