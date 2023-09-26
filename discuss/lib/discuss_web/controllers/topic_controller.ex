92917222447e6a98f2fdd563e927ae8dffb7f6d8
Jayme Weiss <jayme.weiss@zappistore.com>
2023-09-26 21:46:47 +0200

    create and new index page

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,14 +1,35 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
-  alias Discuss.Topic
+  alias DiscussWeb.Router.Helpers, as: Routes
+  alias Discuss.{Repo,Topic}
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{})
     render conn, :new, changeset: changeset
   end
 
-  def create(conn, %{topic: topic_params}) do
-    changeset = Topic.changeset(%Topic{})
-    render conn, :new, changeset: changeset
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, :index, topics: topics
+  end
+
+  def create(conn, %{"topic" => topic_params}) do
+    # We use an empty record here because we are creating one here - would have included an id if we were updating an existing one
+    result = %Topic{}
+    |> Topic.changeset(topic_params)
+    |> Repo.insert()
+
+    case result do
+      {:ok, topic} ->
+        conn
+        |> put_flash(:info, "Topic created successfully.")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Something went wrong when creating topic.")
+        |> render(:new, changeset: changeset)
+
+    end
   end
 end
